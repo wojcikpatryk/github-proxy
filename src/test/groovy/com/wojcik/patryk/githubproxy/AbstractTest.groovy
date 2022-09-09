2c0db07049720e25d1628f566f0e0d72e6876eb5
wojcikpatryk <wojcik.patrykk@gmail.com>
2022-09-09 18:59:22 +0200

    Remove unnecessary annotation from AbstractTest class

--- src/test/groovy/com/wojcik/patryk/githubproxy/AbstractTest.groovy
+++ src/test/groovy/com/wojcik/patryk/githubproxy/AbstractTest.groovy
@@ -1,12 +1,10 @@
 package com.wojcik.patryk.githubproxy
 
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import spock.lang.Specification
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@AutoConfigureWebTestClient(timeout = "PT60S")
 class AbstractTest extends Specification {
 }
